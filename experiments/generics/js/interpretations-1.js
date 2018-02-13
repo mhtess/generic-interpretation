4fd43a3fb55669590b31caae99343dca89ffc829
mhtess <mtessler@stanford.edu>
2018-02-13 14:06:30 -0800

    add unique turker stuff to js/html

--- experiments/generics/js/interpretations-1.js
+++ experiments/generics/js/interpretations-1.js
@@ -190,6 +190,17 @@ function make_slides(f) {
 /// init ///
 function init() {
 
+
+  repeatWorker = false;
+  (function(){
+      var ut_id = "mht-genint-20180213";
+      if (UTWorkerLimitReached(ut_id)) {
+        $('.slide').empty();
+        repeatWorker = true;
+        alert("You have already completed the maximum number of HITs allowed by this requester. Please click 'Return HIT' to avoid any impact on your approval rating.");
+      }
+  })();
+
 exp.numTrials = stim_properties.length;
 
 var creatures = _.map(_.shuffle(creatureNames).slice(0,exp.numTrials),
