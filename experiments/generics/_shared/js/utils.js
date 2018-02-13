bbc18c9e1c3e1234cbc7c699bda243dd883d024a
mhtess <mtessler@stanford.edu>
2018-02-13 11:28:11 -0800

    add uppercase fn; fix paths in html

--- experiments/generics/_shared/js/utils.js
+++ experiments/generics/_shared/js/utils.js
@@ -7,7 +7,11 @@ var utils = {
 		}
 		return arr;
 	},
-	
+
+	upperCaseFirst: function(str){
+		return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	},
+
 	showSlide: function(slidename) {
 			$(".slide").hide();
 			$(".fullslide").hide();
